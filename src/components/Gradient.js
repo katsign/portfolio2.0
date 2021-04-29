c8b26f7d3f41ffef5f4f877d78c31c7d108b3c3a
Kat Buchanan <katrbuch@gmail.com>
2021-04-29 14:01:29 -0500

    gradient wrapper

---
+++ src/components/Gradient.js
@@ -0,0 +1,12 @@
+import '../assets/css/gradient.css'
+import Main from "../components/Main";
+
+const Gradient = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+};
+
+export default Gradient;
