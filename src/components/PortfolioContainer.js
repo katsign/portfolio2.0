8bd3f2521fd47e2daa1056f812b26f47029f688b
Kat Buchanan <katrbuch@gmail.com>
2021-04-29 10:08:06 -0500

    portfolio constructor

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Journal from './pages/Journal';
+import Resume from './pages/Resume';
+
+class Portfolio extends Component {
+  state = {
+    currentPage: 'About'
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === 'About') {
+      return <About />;
+    } else if (this.state.currentPage === 'Projects') {
+      return <Projects />;
+    } else if (this.state.currentPage === 'Journal') {
+      return <Journal />;
+    } else {
+      return <Resume />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
