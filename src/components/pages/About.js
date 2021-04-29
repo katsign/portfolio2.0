3a5afa2a9244d15965f826a3605e336a8cfcd147
Kat Buchanan <katrbuch@gmail.com>
2021-04-29 10:58:13 -0500

    about content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React from 'react';
+import Image from '../../assets/images/self.jpg';
 
 const About = () => (
   <div className="mt-4">
-    <h1>About</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
+    <h2>About</h2>
+    <hr></hr>
+    <div className="my-2" id="bioWrapper">
+      <img
+        width="260"
+        className="float-right m-4 rounded"
+        src={Image}
+        alt="Self portrait of Kat R. Buchanan, 27 years old, caucasian brunette."
+      />
+      <p className="lead mt-4">
+        <span style={{ fontWeight: 400 }}>Meet</span> Kat Ruth Buchanan
+        <span className="material-icons thistle">arrow_downward</span>
+      </p>
+      <div className="badge badge-light p-2 mb-4">
+        Full-Stack Web Developer, emphasis on UI Design 🌞
+      </div>
+      <p>Hey, I'm Kat. A digitally-native digital creative.</p>
+      <p className="text-justify">
+        I'm a full-stack web developer with an emphasis on design, front-end UIs
+        and simplified user experiences. I've worked for a decade in different
+        fields, soaking up experiences — from stomping grapes during the
+        California harvest to interviewing indie musicians backstage; from
+        designing a homestyle Italian cookbook to documenting life in the
+        southern Brazilian jungle. I got my start in journalism and taught
+        myself the real elements of style from there. Coming from this rich and
+        varied background, I'm stoked to see what I can do as a fresh eye in
+        tech. I'm really just a curious young millennial who fondly recalls the
+        old web and fiendishly longs for the new.
+        <span style={{ fontWeight: 400 }}> So let's build it.</span>
+      </p>
+    </div>
+    <div class="row">
+      <div class="col d-flex align-items-center justify-content-center">
+        <div class="badge badge-light p-2">HTML5 + CSS3</div>
+        <div class="badge badge-light p-2">SCSS</div>
+        <div class="badge badge-light p-2">Figma + Adobe CC</div>
+        <div class="badge badge-light p-2">JavaScript</div>
+        <div class="badge badge-light p-2">Node + Express</div>
+        <div class="badge badge-light p-2">MySQL + NoSQL</div>
+        <div class="badge badge-light p-2">React.js</div>
+      </div>
+    </div>
   </div>
 );
 
