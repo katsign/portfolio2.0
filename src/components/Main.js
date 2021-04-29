7416a71f170b591f82e6467f93d373d5a495572b
Kat Buchanan <katrbuch@gmail.com>
2021-04-29 10:09:04 -0500

    main wrapper

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import Portfolio from "./PortfolioContainer"
+
+const Main = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-12">
+                    <Portfolio />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
