50a7704b89793115a0af73591c66456d3b6bceff
Kat Buchanan <katrbuch@gmail.com>
2021-04-29 18:12:09 -0500

    swapped imgs

--- src/components/pages/Journal.js
+++ src/components/pages/Journal.js
@@ -12,8 +12,8 @@ const Journal = () => (
     <h2>Visual Journal <span style={{color: 'thistle'}}>Spring 2021</span></h2>
     <hr></hr>
     <div className="d-flex flex-row justify-content-around mb-4">
-      <JournalImage image={IMG3} /> <JournalImage image={IMG5} />
-      <JournalImage image={IMG1} /> <JournalImage image={IMG2} />
+      <JournalImage image={IMG3} /> <JournalImage image={IMG1} />
+      <JournalImage image={IMG5} /> <JournalImage image={IMG2} />
       <JournalImage image={IMG6} /> <JournalImage image={IMG4} />
     </div>
   </div>
