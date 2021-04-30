bc9552f4349179c962622676e41d187658b905f9
Kat Buchanan <katrbuch@gmail.com>
2021-04-30 10:38:48 -0500

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import Icon from '../assets/images/favicon.ico';
+
+const Header = () => {
+  return (
+    <div>
+      <img
+        width="44"
+        src={Icon}
+        alt="Logo"
+        id="logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
