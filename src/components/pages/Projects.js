a62f03f82bd3ab9b6e5394f274f3bd72c61ad091
Kat Buchanan <katrbuch@gmail.com>
2021-07-09 15:42:30 -0500

    added pill nav to projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,4 +1,5 @@
 import ProjectCard from '../ProjectCard';
+import { Tabs, Tab } from 'react-bootstrap';
 import Devlr from '../../assets/images/ss_dev.PNG';
 import PAIR from '../../assets/images/ss_pair.png';
 import Budget from '../../assets/images/ss_budget.PNG';
@@ -9,54 +10,70 @@ import Users from '../../assets/images/ss_users.PNG';
 const Projects = () => (
   <div className="mt-4" id="proj">
     <h2>Featured Projects</h2>
-    <hr></hr>
-    <div className="card-group mb-1">
-      <ProjectCard
-        link="https://devlrapp.herokuapp.com/"
-        image={Devlr}
-        title="devlr Developer Network"
-        blurb="Create a customized profile to display your development background, pinned projects and status updates to share with other developers worldwide."
-        repo="https://github.com/brandyquinlan/devlr"
-      />
-      <ProjectCard
-        link="https://brandyquinlan.github.io/PAIR/"
-        image={PAIR}
-        title="PAIR Culinary Curation"
-        blurb="Pair is a dynamic and mobile-responsive web application that lets a user search for food and cocktail recipes from separate databases and curate the results in one place."
-        repo="https://github.com/brandyquinlan/PAIR"
-      />
-    </div>
-    <div className="card-group mb-1">
-      <ProjectCard
-        link="https://katsign-budget-tracker.herokuapp.com/"
-        image={Budget}
-        title="Progressive Budget Tracker"
-        blurb="This progressive web app uses MongoDB and IndexedDB to let a user add expenses and deposits to their budget on the go, with or without a connection."
-        repo="https://github.com/katsign/pwa-budget"
-      />
-      <ProjectCard
-        link="https://katsign.github.io/devteam/"
-        image={Devteam}
-        title="DevTeam Profile Generator"
-        blurb="This Node.js command line application generates a styled HTML webpage containing your development team's contact info and professional roles."
-        repo="https://github.com/katsign/devteam"
-      />
-    </div>
-    <div className="card-group mb-1">
-      <ProjectCard
-        link="https://katsign-gotta-eat.herokuapp.com/"
-        image={Burger}
-        title="Express Burger Log"
-        blurb="This simple full-stack application uses Node.js, Express, a MySQL database and the templating engine Handlebars to display logs of newly-ordered and devoured burgers."
-        repo="https://github.com/katsign/gotta-eat"
-      />
-      <ProjectCard
-        link="https://katsign.github.io/react-user-directory/"
-        image={Users}
-        title="React User Directory"
-        blurb="This web app lets a user view, sort and filter through a directory of users from the Random User Generator API using state management in React."
-        repo="https://github.com/katsign/react-user-directory"
-      />
+    <hr />
+    <div className="d-flex flex-row align-items-center justify-content-around">
+      <Tabs defaultActiveKey="webdev" variant="pills" id="projectFeeds">
+        <Tab eventKey="webdev" title="Web Development">
+          <hr />
+          <div className="card-group mb-1">
+            <ProjectCard
+              link="https://devlrapp.herokuapp.com/"
+              image={Devlr}
+              title="devlr Developer Network"
+              blurb="Create a customized profile to display your development background, pinned projects and status updates to share with other developers worldwide."
+              repo="https://github.com/brandyquinlan/devlr"
+            />
+            <ProjectCard
+              link="https://brandyquinlan.github.io/PAIR/"
+              image={PAIR}
+              title="PAIR Culinary Curation"
+              blurb="Pair is a dynamic and mobile-responsive web application that lets a user search for food and cocktail recipes from separate databases and curate the results in one place."
+              repo="https://github.com/brandyquinlan/PAIR"
+            />
+          </div>
+          <div className="card-group mb-1">
+            <ProjectCard
+              link="https://katsign-budget-tracker.herokuapp.com/"
+              image={Budget}
+              title="Progressive Budget Tracker"
+              blurb="This progressive web app uses MongoDB and IndexedDB to let a user add expenses and deposits to their budget on the go, with or without a connection."
+              repo="https://github.com/katsign/pwa-budget"
+            />
+            <ProjectCard
+              link="https://katsign.github.io/devteam/"
+              image={Devteam}
+              title="DevTeam Profile Generator"
+              blurb="This Node.js command line application generates a styled HTML webpage containing your development team's contact info and professional roles."
+              repo="https://github.com/katsign/devteam"
+            />
+          </div>
+          <div className="card-group mb-1">
+            <ProjectCard
+              link="https://katsign-gotta-eat.herokuapp.com/"
+              image={Burger}
+              title="Express Burger Log"
+              blurb="This simple full-stack application uses Node.js, Express, a MySQL database and the templating engine Handlebars to display logs of newly-ordered and devoured burgers."
+              repo="https://github.com/katsign/gotta-eat"
+            />
+            <ProjectCard
+              link="https://katsign.github.io/react-user-directory/"
+              image={Users}
+              title="React User Directory"
+              blurb="This web app lets a user view, sort and filter through a directory of users from the Random User Generator API using state management in React."
+              repo="https://github.com/katsign/react-user-directory"
+            />
+          </div>
+        </Tab>
+        <Tab eventKey="graphics" title="Graphic Design">
+          <hr />
+        </Tab>
+        <Tab eventKey="photo" title="Photography">
+          <hr />
+        </Tab>
+        <Tab eventKey="published" title="Published">
+          <hr />
+        </Tab>
+      </Tabs>
     </div>
   </div>
 );
