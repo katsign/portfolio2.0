7e0972680d658f80ebc76c16e4f3beb4883b953c
Kat Buchanan <katrbuch@gmail.com>
2021-07-10 16:42:57 -0500

    placed scroll to top

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -1,10 +1,12 @@
 import '../assets/css/gradient.css'
 import Main from "../components/Main";
+import ScrollToTop from "../components/ScrollToTopArrow"
 
 const Gradient = () => {
   return (
     <div>
       <Main />
+      <ScrollToTop />
     </div>
   );
 };
